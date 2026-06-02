79f29614a692ccf517091ca0fb521ec403447b1a
Mohammed-Moshood <mohammedmoshood100@gmail.com>
2026-06-02 22:33:34 +0100

    fix: countdown not showing - wrap in DOMContentLoaded

--- routes/wallet.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/walletController');
-
-router.post('/connect',    ctrl.connectWallet);
-router.post('/disconnect', ctrl.disconnectWallet);
-router.get('/profile',     ctrl.getProfile);
-
-module.exports = router;
